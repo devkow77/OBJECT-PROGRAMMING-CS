58e95af2fde3f4c4de0189936451dd2562708449
devkow77 <kowalsky429@gmail.com>
2025-04-12 17:50:52 +0200

    add lab9 (simple windows form app)

---
+++ LAB9/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace LAB9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.header = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.repeatPassword = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.repeatPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.createButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.header2 = new System.Windows.Forms.Label();
+            this.usersTable = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // header
+            // 
+            this.header.AutoSize = true;
+            this.header.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.header.Location = new System.Drawing.Point(96, 49);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(205, 29);
+            this.header.TabIndex = 0;
+            this.header.Text = "Create User Form";
+            // 
+            // email
+            // 
+            this.email.AutoSize = true;
+            this.email.Location = new System.Drawing.Point(77, 163);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(40, 16);
+            this.email.TabIndex = 1;
+            this.email.Text = "email";
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Location = new System.Drawing.Point(77, 206);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(67, 16);
+            this.username.TabIndex = 2;
+            this.username.Text = "username";
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(77, 248);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(66, 16);
+            this.password.TabIndex = 3;
+            this.password.Text = "password";
+            // 
+            // repeatPassword
+            // 
+            this.repeatPassword.AutoSize = true;
+            this.repeatPassword.Location = new System.Drawing.Point(77, 291);
+            this.repeatPassword.Name = "repeatPassword";
+            this.repeatPassword.Size = new System.Drawing.Size(108, 16);
+            this.repeatPassword.TabIndex = 4;
+            this.repeatPassword.Text = "repeat password";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(213, 157);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(136, 22);
+            this.emailTextBox.TabIndex = 5;
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(213, 200);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(136, 22);
+            this.usernameTextBox.TabIndex = 6;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(213, 242);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(136, 22);
+            this.passwordTextBox.TabIndex = 7;
+            // 
+            // repeatPasswordTextBox
+            // 
+            this.repeatPasswordTextBox.Location = new System.Drawing.Point(212, 285);
+            this.repeatPasswordTextBox.Name = "repeatPasswordTextBox";
+            this.repeatPasswordTextBox.PasswordChar = '*';
+            this.repeatPasswordTextBox.Size = new System.Drawing.Size(136, 22);
+            this.repeatPasswordTextBox.TabIndex = 8;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(80, 105);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(232, 23);
+            this.progressBar.TabIndex = 9;
+            // 
+            // createButton
+            // 
+            this.createButton.BackColor = System.Drawing.Color.Lime;
+            this.createButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.createButton.Location = new System.Drawing.Point(80, 349);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(105, 30);
+            this.createButton.TabIndex = 10;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = false;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.Color.Red;
+            this.resetButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.resetButton.Location = new System.Drawing.Point(208, 349);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(105, 30);
+            this.resetButton.TabIndex = 11;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // header2
+            // 
+            this.header2.AutoSize = true;
+            this.header2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.header2.Location = new System.Drawing.Point(513, 49);
+            this.header2.Name = "header2";
+            this.header2.Size = new System.Drawing.Size(107, 29);
+            this.header2.TabIndex = 12;
+            this.header2.Text = "User List";
+            // 
+            // usersTable
+            // 
+            this.usersTable.AutoScroll = true;
+            this.usersTable.AutoSize = true;
+            this.usersTable.ColumnCount = 3;
+            this.usersTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 43.34471F));
+            this.usersTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 56.65529F));
+            this.usersTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 116F));
+            this.usersTable.Location = new System.Drawing.Point(420, 105);
+            this.usersTable.Name = "usersTable";
+            this.usersTable.Padding = new System.Windows.Forms.Padding(3);
+            this.usersTable.RowCount = 1;
+            this.usersTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.usersTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.usersTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.usersTable.Size = new System.Drawing.Size(361, 57);
+            this.usersTable.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 450);
+            this.Controls.Add(this.usersTable);
+            this.Controls.Add(this.header2);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.repeatPasswordTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.repeatPassword);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.header);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label header;
+        private System.Windows.Forms.Label email;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.Label repeatPassword;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox repeatPasswordTextBox;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label header2;
+        private System.Windows.Forms.TableLayoutPanel usersTable;
+    }
+}
+
---
+++ LAB9/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB9
+{
+    public partial class Form1: Form
+    {
+        private List<User> users = new List<User>();
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+            // Podpięcie zdarzeń do wszystkich pól tekstowych
+            emailTextBox.TextChanged += InputFields_TextChanged;
+            usernameTextBox.TextChanged += InputFields_TextChanged;
+            passwordTextBox.TextChanged += InputFields_TextChanged;
+            repeatPasswordTextBox.TextChanged += InputFields_TextChanged;
+
+            progressBar.Maximum = 100;
+            progressBar.Value = 0;
+
+            // Utworzenie tabeli do wyświetlania użytkowników
+            usersTable.RowCount = 1;
+            usersTable.ColumnCount = 3;
+
+            usersTable.Controls.Add(new Label() { Text = "Email", AutoSize = true }, 0, 0);
+            usersTable.Controls.Add(new Label() { Text = "Username", AutoSize = true }, 1, 0);
+            usersTable.Controls.Add(new Label() { Text = "Password", AutoSize = true }, 2, 0);
+        }
+
+        public class User
+        {
+            public string Email { get; }
+            public string Username { get; }
+            private string password;
+            
+            public User(string email, string username, string password)
+            {
+                Email = email;
+                Username = username;
+                this.password = password;
+            }
+
+
+
+            public override string ToString()
+            {
+                return $"Email: {Email} Username: {Username} Password: {password}";
+            }
+        }
+
+        public bool isValidEmail(string email)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(email);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        public void clearFields()
+        {
+            emailTextBox.Text = "";
+            usernameTextBox.Text = "";
+            passwordTextBox.Text = "";
+            repeatPasswordTextBox.Text = "";
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            string email = emailTextBox.Text;
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+            string repeatPassword = repeatPasswordTextBox.Text;
+
+            try
+            {
+                if (email == "" || username == "" || password == "" || repeatPassword == "")
+                {
+                    throw new ArgumentNullException("All fields are required.");
+                }
+                else if (!isValidEmail(email)) throw new Exception("Invalid email format");
+                else if (password != repeatPassword) throw new Exception("Passwords must be the same!");
+                else
+                {
+                    foreach (User user in users)
+                    {
+                        if (user.Email == email || user.Username == username)
+                        {
+                            throw new Exception("Email or username already exists");
+                        }
+                    }
+
+                    Console.WriteLine("User has been created!");
+                    users.Add(new User(email, username, password));
+                    clearFields();
+
+                    int rowIndex = usersTable.RowCount;
+
+                    // dodanie nowego usera do tabeli
+                    usersTable.RowCount++;
+                    usersTable.Controls.Add(new Label() { Text = email, AutoSize = true }, 0, rowIndex);
+                    usersTable.Controls.Add(new Label() { Text = username, AutoSize = true }, 1, rowIndex);
+                    usersTable.Controls.Add(new Label() { Text = password, AutoSize = true }, 2, rowIndex);
+                }      
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }      
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            clearFields();
+        }
+
+        private void InputFields_TextChanged(object sender, EventArgs e)
+        {
+            int filled = 0;
+
+            if (!string.IsNullOrWhiteSpace(emailTextBox.Text)) filled++;
+            if (!string.IsNullOrWhiteSpace(usernameTextBox.Text)) filled++;
+            if (!string.IsNullOrWhiteSpace(passwordTextBox.Text)) filled++;
+            if (!string.IsNullOrWhiteSpace(repeatPasswordTextBox.Text)) filled++;
+
+            progressBar.Value = filled * 25;
+        }
+    }
+}
